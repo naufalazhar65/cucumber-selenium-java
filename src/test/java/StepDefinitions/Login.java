bd9609459dc60db9ac6badb6dd00d55ae79bde09
Naufal Azhar <naufalazhar65@gmail.com>
2023-03-26 16:01:54 +0700

    first commit

--- src/test/java/StepDefinitions/Login.java
+++ src/test/java/StepDefinitions/Login.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -18,17 +20,26 @@ public class Login {
 
 	WebDriver driver = null;
 
+    @Before
+    public void setUp() {
+        // Set up Chrome driver and wait times
+        System.setProperty("webdriver.chrome.driver", "/Users/naufalazhar/Documents/ChromeDriver/chromedriver");
+        System.setProperty("webdriver.chrome.silentOutput", "true");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+        // Close the driver after each test case
+        driver.quit();
+    }
+	
 	@Given("Open web url {string}")
 	public void open_web_url(String url) {
-
-		System.setProperty("webdriver.chrome.driver", "/Users/naufalazhar/Documents/ChromeDriver/chromedriver");
-		System.setProperty("webdriver.chrome.silentOutput", "true");
-		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
-
-		driver.get("https://www.saucedemo.com/");
+		driver.get(url);
 		String ActualTitle = driver.getTitle();
 		String ExpectedTitle = "Swag Labs";
 		Assert.assertEquals(ExpectedTitle, ActualTitle);
@@ -42,28 +53,21 @@ public class Login {
 		Thread.sleep(2000);
 		driver.findElement(By.id("password")).sendKeys(password);
 		Thread.sleep(2000);
-
 	}
 
 	@When("Click Login button")
 	public void click_Login_button() {
-		driver.findElement(By.id("login-button")).click();
+		driver.findElement(By.id("login-button")).click();	
 	}
 
 	@Then("Should success login and redirected to homepage")
 	public void should_success_login_and_redirected_to_homepage() {
 		Assert.assertTrue(driver.findElement(By.id("inventory_container")).isDisplayed());
-		driver.close();
-		driver.quit();
 	}
 
 	@Then("Failed login and showing message")
 	public void Failed_login_and_showing_message() {
 		Assert.assertTrue(driver.findElement(By.xpath("//div[@class='error-message-container error']")).isDisplayed());
-	
-		driver.close();
-		driver.quit();
-
 	}
-
+	
 }
